8bbffcdc005af870fc080e94cc971001c3a5a48e
Gregorius Soedharmo <arkatufus@yahoo.com>
2024-12-24 00:22:34 +0700

    Fix `Akka.Util.Result` edge case (#7433)
    Co-authored-by: Aaron Stannard <aaron@petabridge.com>

--- src/core/Akka/Util/Result.cs
+++ src/core/Akka/Util/Result.cs
@@ -137,7 +137,30 @@ namespace Akka.Util
         /// <returns>TBD</returns>
         public static Result<T> FromTask<T>(Task<T> task)
         {
-            return task.IsCanceled || task.IsFaulted ? new Result<T>(task.Exception) : new Result<T>(task.Result);
+            if(!task.IsCompleted)
+                throw new ArgumentException("Task is not completed. Result.FromTask only accepts completed tasks.", nameof(task));
+            
+            if(task.Exception is not null)
+                return new Result<T>(task.Exception);
+            
+            if (task.IsCanceled && task.Exception is null)
+            {
+                try
+                {
+                    _ = task.GetAwaiter().GetResult();
+                }
+                catch(Exception e)
+                {
+                    return new Result<T>(e);
+                }
+
+                throw new InvalidOperationException("Should never reach this line!");
+            }
+            
+            if(task.IsFaulted && task.Exception is null)
+                throw new InvalidOperationException("Should never happen! something is wrong with .NET Task code!");
+            
+            return new Result<T>(task.Result);
         }
 
         /// <summary>
