1f7ffa7479152b13f201434a6791156f7f18d213
Aaron Stannard <aaron@petabridge.com>
2025-01-09 18:22:54 -0600

    Added 2025 copyright headers (#7454)
    * limit Lightbend copyright to 2022, since we stopped porting changes from beyond that
    
    * added 2025 copyright headers to all files
    
    * fixed missing `using` statements

--- src/contrib/testkits/Akka.TestKit.Xunit2.Tests/Internals/AkkaEqualExceptionSpec.cs
+++ src/contrib/testkits/Akka.TestKit.Xunit2.Tests/Internals/AkkaEqualExceptionSpec.cs
@@ -1,9 +1,9 @@
-﻿// -----------------------------------------------------------------------
-//  <copyright file="AkkaEqualException.cs" company="Akka.NET Project">
-//      Copyright (C) 2009-2025 Lightbend Inc. <http://www.lightbend.com>
-//      Copyright (C) 2013-2025 .NET Foundation <https://github.com/akkadotnet/akka.net>
-//  </copyright>
-// -----------------------------------------------------------------------
+﻿//-----------------------------------------------------------------------
+// <copyright file="AkkaEqualExceptionSpec.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2022 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2025 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
 
 using Akka.TestKit.Xunit2.Internals;
 using Xunit;
--- src/core/Akka.Persistence.Tests/MultipleRecoveryPermitterSpec.cs
+++ src/core/Akka.Persistence.Tests/MultipleRecoveryPermitterSpec.cs
@@ -1,9 +1,9 @@
-﻿// -----------------------------------------------------------------------
-//  <copyright file="MultipleRecoveryPermitterSpec.cs" company="Akka.NET Project">
-//      Copyright (C) 2009-2025 Lightbend Inc. <http://www.lightbend.com>
-//      Copyright (C) 2013-2025 .NET Foundation <https://github.com/akkadotnet/akka.net>
-//  </copyright>
-// -----------------------------------------------------------------------
+﻿//-----------------------------------------------------------------------
+// <copyright file="MultipleRecoveryPermitterSpec.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2022 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2025 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
 
 using System.Threading.Tasks;
 using Akka.Actor;
